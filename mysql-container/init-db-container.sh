d630d1bc4dcf22e86ad1cd89674bb35e29eff688
cloudapps-idey <cloudnative-appdev@outlook.com>
2023-03-24 20:25:50 -0400

    took out mysql container folder

--- mysql-container/init-db-container.sh
+++
@@ -1,1 +0,0 @@
-oc new-app -e MYSQL_USER=user1  -e MYSQL_PASSWORD=password  -e MYSQL_DATABASE=bookstore  -e MYSQL_ROOT_PASSWORD=password registry.access.redhat.com/rhel8/mysql-80 --name mysql
--- mysql-container/insert-book-data.sql
+++
@@ -1,6 +0,0 @@
-USE bookstore;
-INSERT INTO `book` (`id`, `author`, `category`, `name`, `pages`, `price`, `publication`,`year`) VALUES
-(1, 'Santosh Kumar', 'Programming', 'Extensive Multithreading', 100, 50, 'Mcgraw Publication', '2015'),
-(3, 'Suchitra', 'Programming', 'Java', 1000, 200, 'Mcgraw Publication', '2016'),
-(5, 'Nitesh', 'Computer Programming', 'C++', 1500, 240, 'Rajput Publication', '2017'),
-(7, 'Indrani', 'Sample Book', 'C++', 1500, 240, 'ORiley Publication', '2018');
