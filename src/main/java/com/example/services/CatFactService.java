2790d89407e203851990da294b247d2b0e733b92
cloudapps-idey <cloudnative-appdev@outlook.com>
2023-03-24 20:55:53 -0400

    took out all references of mysql db

--- src/main/java/com/example/services/CatFactService.java
+++ src/main/java/com/example/services/CatFactService.java
@@ -10,7 +10,7 @@ import java.nio.charset.Charset;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+//   import org.apache.tomcat.util.http.fileupload.IOUtils;
 //import org.apache.logging.log4j.core.util.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
